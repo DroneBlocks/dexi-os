87d34b4fbf499449ce0731114c4d80481c998958
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-07-12 16:19:46 -0500

    Set hostname to dexi ssid

--- resources/first_boot.sh
+++ resources/first_boot.sh
@@ -5,6 +5,8 @@ FULL_MAC=$(cat /sys/class/net/wlan0/address)
 PARTIAL_MAC=$(echo $FULL_MAC | awk -F: '{print $(NF-1)$NF}')
 DEXI_SSID="dexi_$PARTIAL_MAC"
 
+hostnamectl hostname $DEXI_SSID
+
 #setup the wifi stuff but use placeholders for first boot
 /home/dexi/wifi_utilities/setup_wlan_and_AP_modes.sh -d -s XXXXXXXX -p XXXXXXXX  -a $DEXI_SSID -r droneblocks
 
