80fd13cba453385b9e88d0c87c3def3dcaf2163d
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-10-25 16:57:51 -0500

    Do correct volume mapping

--- resources/first_boot.sh
+++ resources/first_boot.sh
@@ -23,8 +23,8 @@ chown -R dexi:dexi /home/dexi
 docker load < /home/dexi/docker-drag/droneblocks_dexi-droneblocks.tar
 docker load < /home/dexi/docker-drag/droneblocks_dexi-node-red.tar
 docker run -d --restart unless-stopped -p 80:80 --name dexi-droneblocks droneblocks/dexi-droneblocks:latest
-docker run -d --restart unless-stopped -p 1880:1880 /home/dexi/node-red-dexi/flows:/data --name dexi-node-red droneblocks/dexi-node-red:latest
-#rm -rf /home/dexi/docker-drag
+docker run -d --restart unless-stopped -p 1880:1880 -v /home/dexi/node-red-dexi/flows:/data --name dexi-node-red droneblocks/dexi-node-red:latest
+rm -rf /home/dexi/docker-drag
 
 # Boot faster
 systemctl disable systemd-networkd-wait-online.service
