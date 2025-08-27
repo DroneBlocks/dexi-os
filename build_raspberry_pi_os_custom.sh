e281e7543d4aab6e6f0a177331a5092196871811
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-08-27 13:51:09 -0500

    Remove custom provision build

--- build_raspberry_pi_os_custom.sh
+++
@@ -1,2 +0,0 @@
-# https://github.com/mkaczanowski/packer-builder-arm
-sudo docker run --rm --privileged -v /dev:/dev -v ${PWD}:/build mkaczanowski/packer-builder-arm:latest build ./raspberry_pi_os_custom.pkr.hcl 
